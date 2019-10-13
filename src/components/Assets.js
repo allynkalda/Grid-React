d4c31bfd8e82735fc1f8fa3375841eb26f682896
Allyn Alda <Teddy@om.home>
2019-10-13 18:39:53 +0200

    basic styling

--- src/components/Assets.js
+++ src/components/Assets.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { useFetch } from '../utils/fetch';
 import { assetsUrl } from '../config/config';
 import Header from './Header';
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"; 
+import Loader from 'react-loader-spinner';
 
 export default function Assets() {
 
@@ -11,10 +13,19 @@ const headers = data && data.assets ? Object.keys(data.assets[0]) : null;
 const body = data && data.assets ? data.assets : null
 const showButton = true;
 
-console.log(loading)
     return (
         <div>
-            <Header headers={headers} body={body} showButton={showButton}></Header>
+            {
+            loading ?
+            <div className="loader">
+            <Loader className="loader-inner"
+                type="Puff"
+                color="#00BFFF"
+                height={100}
+                width={100}
+             /></div> :
+             <Header headers={headers} body={body} showButton={showButton}></Header>
+            }
         </div>
     )
 }
--- src/components/Entities.js
+++ src/components/Entities.js
@@ -4,25 +4,37 @@ import { useFetch } from '../utils/fetch';
 import { entitiesUrl } from '../config/config';
 import Header from './Header';
 import ContextMenu from './ContextMenu';
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"; 
+import Loader from 'react-loader-spinner';
 
 function Entities() {
 
     const [ id, setId ] = useState(null);
-    const data = useFetch(entitiesUrl)
+    const data = useFetch(entitiesUrl);
+    const loading = data[1];
     const headers = data && data[0] ? Object.keys(data[0].entities[0]) : null;
     const body = data && data[0] ? data[0].entities : null;
     const showButton = false;
-
     const rowClick = (item) => {
         setId(item)
     }
 
     return (
         <div>
+            {
+            loading ?
+            <div className="loader">
+            <Loader className="loader-inner"
+                type="Puff"
+                color="#00BFFF"
+                height={100}
+                width={100}
+             /></div> :
             <ContextMenu id={id}>
-            <Header rowClick={rowClick}
-            headers={headers} body={body} showButton={showButton}></Header>
+                <Header rowClick={rowClick}
+                headers={headers} body={body} showButton={showButton}></Header>
             </ContextMenu>
+            }
         </div>
     )
 }
